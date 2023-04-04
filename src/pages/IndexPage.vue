7cf6071e29545036cdc32200041b4f387a052ce1
Hermann EHO <hermannleboss@gmail.com>
2023-04-04 08:30:47 +0000

    feat: firebase for auth

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,29 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+
+
+    <div v-if="isAuthenticated">
+      user
+      <div>
+        <button @click="logout">
+          Log Out
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      <q-btn @click="signIn">
+        Sign In with Google
+      </q-btn>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+import {useFirebase} from 'src/composables/firebase';
+import {useAuth} from '@vueuse/firebase/useAuth';
+import {GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+const {auth} = useFirebase()
+const {isAuthenticated, user} = useAuth(auth)
+const signIn = () => signInWithPopup(auth, new GoogleAuthProvider())
+const logout = () => signOut(auth)
 </script>

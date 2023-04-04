5eced4e9591f704b49f6b25bebef0e6827f4c8a5
Hermann EHO <hermannleboss@gmail.com>
2023-04-04 13:20:39 +0000

    feat: layout add drawer to layout with logout button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,10 +5,42 @@
         <q-toolbar-title class="text-center" >
           Crypto Tips
         </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+      <q-item-label
+        header
+      >
+        Essential Links
+      </q-item-label>
+      <div v-if="isAuthenticated">
+        <div>{{user.email}}</div>
+        <button @click="logout">
+          Log Out
+        </button>
+      </div>
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+
+<script setup lang="ts">
+import {ref} from 'vue';
+import {useFirebase} from 'src/composables/firebase';
+import {useAuth} from '@vueuse/firebase';
+import {signOut} from 'firebase/auth';
+
+const rightDrawerOpen = ref(false)
+const {auth} = useFirebase()
+const {isAuthenticated, user} = useAuth(auth)
+function toggleRightDrawer () {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
+const logout = () => signOut(auth)
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,9 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div v-if="isAuthenticated">
-      user
-      <div>
-        <button @click="logout">
-          Log Out
-        </button>
-      </div>
+      <button @click="logout">
+        Log Out
+      </button>
     </div>
     <div v-else>
       <q-btn @click="signIn">
@@ -18,23 +15,31 @@
 
 <script setup lang="ts">
 import {useFirebase} from 'src/composables/firebase';
-import {useAuth} from '@vueuse/firebase/useAuth';
+import {useFirestore, useAuth} from '@vueuse/firebase';
+import {doc, setDoc, collection, where, query} from 'firebase/firestore'
 import {GoogleAuthProvider, signInWithPopup, signOut, getAdditionalUserInfo} from 'firebase/auth'
+import {computed} from 'vue';
 
-import { doc, setDoc} from 'firebase/firestore'
 
 const {auth, db} = useFirebase()
-const {isAuthenticated} = useAuth(auth)
+const {isAuthenticated, user} = useAuth(auth)
 const signIn = () => signInWithPopup(auth, new GoogleAuthProvider()).then(
   async (result) => {
     const isNewUser = getAdditionalUserInfo(result)?.isNewUser
-    console.log(result.user)
     const {email, displayName, photoURL, uid} = result.user
 
     if (isNewUser) {
       await setDoc(doc(db, 'users', uid), {email, displayName, photoURL})
     }
+    console.log(user.value)
   }
 )
+
+let teamsQuery = computed(() =>
+  query(collection(db, 'teams'), where('userId', '==', user.value ? user.value.uid : ''))
+)
+
+const teams = useFirestore(teamsQuery)
+console.log(teams.value)
 const logout = () => signOut(auth)
 </script>

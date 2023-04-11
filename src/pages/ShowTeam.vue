ddbb877f125f5b1d8cd0cf4c323f2724df80919c
Hermann EHO <hermannleboss@gmail.com>
2023-04-11 23:48:03 +0000

    feat: remove isloading

--- src/pages/ShowTeam.vue
+++ src/pages/ShowTeam.vue
@@ -1,12 +1,19 @@
 <template>
   <AppHeader
-    :title="`Team :  ${!isLoading && docs ? docs.name : ''}`"
+    :title="`Team :  ${teamData ? teamData.name : ''}`"
     back_link="/"
     @toggleRightDrawer="appStore.toggleDrawer()"
   />
   <q-page-container>
-    <div class="q-pa-md">ds</div>
-    {{ teamData }}
+    <div class="q-ml-sm" v-if="teamData">
+      <p>Team : {{ teamData.name }}</p>
+      <p>Description : {{ teamData.description }}</p>
+      <p>Members : {{ teamData.members.length }}</p>
+      <p>Member list</p>
+      <ul>
+        <li v-for="member in teamData.members" :key="member">{{ member }}</li>
+      </ul>
+    </div>
   </q-page-container>
 </template>
 
@@ -16,23 +23,22 @@ import { useAppStore } from 'src/stores'
 import { useFirebase } from 'src/composables/firebase'
 import { useAuth, useFirestore } from '@vueuse/firebase'
 import { useRouter } from 'vue-router'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { doc } from 'firebase/firestore'
 
-const isLoading = ref(true)
 const router = useRouter()
 if (!router.currentRoute.value.params.ulid) {
   router.push('/404')
 }
 
-const id = router.currentRoute.value.params.ulid
 const appStore = useAppStore()
 const { auth, db } = useFirebase()
 const { isAuthenticated } = useAuth(auth)
-const docs = ref()
-isLoading.value = false
+
+const id = router.currentRoute.value.params.ulid
 const teamQuery = computed(() => doc(db, 'teams', id as string))
 const teamData = useFirestore(teamQuery, null)
+
 if (!isAuthenticated) {
   // TODO redirect to home page
 }

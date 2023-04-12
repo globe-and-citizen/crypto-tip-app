60905f869cee71095c880294d6ad7712c877c1ed
Hermann EHO <hermannleboss@gmail.com>
2023-04-12 10:34:11 +0000

    feat: validation for address

--- src/pages/CreateTeam.vue
+++ src/pages/CreateTeam.vue
@@ -7,51 +7,55 @@
   <q-page-container>
     <div class="q-pa-md">
       <div class="q-gutter-md items-right">
-        <q-input
-          outlined
-          v-model="team.name"
-          label="Team Name"/>
+        <q-input outlined v-model="team.name" label="Team Name" />
         <q-input
           outlined
           v-model="team.description"
           type="textarea"
-          label="Description"/>
+          label="Description"
+        />
         <q-input
           outlined
           v-for="(m, i) in team.members"
           v-model="team.members[i]"
-          :key="i"
-          label="Member Address">
+          :key="m"
+          label="Member Address"
+          :rules="[
+            (value) => isAddress(value) || 'You need to add a valid address',
+          ]"
+        >
           <template v-slot:before>
-            <q-icon name="add" color="primary" @click="addTeamMember(i+1)"/>
-            <q-icon name="remove" v-if="i !== team.members.length - 1"/>
-            <q-icon name="remove" color="red" @click="removeTeamMember(i)"
-                    v-if="team.members.length>1 && i === team.members.length - 1"/>
+            <q-icon name="add" color="primary" @click="addTeamMember(i + 1)" />
+            <q-icon name="remove" v-if="i !== team.members.length - 1" />
+            <q-icon
+              name="remove"
+              color="red"
+              @click="removeTeamMember(i)"
+              v-if="team.members.length > 1 && i === team.members.length - 1"
+            />
           </template>
         </q-input>
         <!--        <q-btn round color="primary" icon="add" class="float-right"/>-->
-        <q-btn class="float-right" @click="createTeam()">
-          Add Team
-        </q-btn>
+        <q-btn class="float-right" @click="createTeam()"> Add Team</q-btn>
       </div>
-
     </div>
   </q-page-container>
 </template>
 
 <script setup lang="ts">
-import AppHeader from 'components/AppHeader.vue';
-import {useAppStore} from 'src/stores';
-import {ref} from 'vue';
-import {useFirebase} from 'src/composables/firebase';
-import {useAuth} from '@vueuse/firebase';
-import {doc, setDoc} from 'firebase/firestore';
-import {ulid} from 'ulid'
-import {useQuasar} from 'quasar';
+import AppHeader from 'components/AppHeader.vue'
+import { useAppStore } from 'src/stores'
+import { ref } from 'vue'
+import { useFirebase } from 'src/composables/firebase'
+import { useAuth } from '@vueuse/firebase'
+import { doc, setDoc } from 'firebase/firestore'
+import { ulid } from 'ulid'
+import { useQuasar } from 'quasar'
+import { ethers } from 'ethers'
 
 const appStore = useAppStore()
-const {auth, db} = useFirebase()
-const {isAuthenticated, user} = useAuth(auth)
+const { auth, db } = useFirebase()
+const { isAuthenticated, user } = useAuth(auth)
 
 const id = ulid()
 const $q = useQuasar()
@@ -65,12 +69,12 @@ const initialTeamValue = {
   description: '',
   address: '',
   members: ['', ''],
-  user: user.value?.uid
+  user: user.value?.uid,
 }
 const team = ref(initialTeamValue)
 
 const addTeamMember = function (index: number) {
-  team.value.members.splice(index, 0, 'new one')
+  team.value.members.splice(index, 0, '')
 }
 
 const removeTeamMember = function (index: number) {
@@ -84,11 +88,13 @@ const removeTeamMember = function (index: number) {
 const createTeam = async function () {
   await setDoc(doc(db, 'teams', id), team.value).then(function () {
     team.value = initialTeamValue
-    $q.notify({type: 'positive', message: 'Team successfully Created'})
+    $q.notify({ type: 'positive', message: 'Team successfully Created' })
   })
+}
+
+const isAddress = (value: never) => {
+  return ethers.isAddress(value)
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

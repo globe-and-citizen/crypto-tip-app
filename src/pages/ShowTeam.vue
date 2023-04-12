d6ce11ee91fff81692719a46d698e71c333794d4
Hermann EHO <hermannleboss@gmail.com>
2023-04-12 11:46:38 +0000

    feat: manage delete team

--- src/pages/ShowTeam.vue
+++ src/pages/ShowTeam.vue
@@ -5,6 +5,18 @@
     @toggleRightDrawer="appStore.toggleDrawer()"
   />
   <q-page-container>
+    <div class="q-ml-sm">
+      <q-icon
+        name="edit"
+        color="primary"
+        @click="
+          () => {
+            console.log()
+          }
+        "
+      />
+      <q-icon name="delete" color="red" @click="deleteTeam()" />
+    </div>
     <div class="q-ml-sm" v-if="teamData">
       <p>Team : {{ teamData.name }}</p>
       <p>Description : {{ teamData.description }}</p>
@@ -16,8 +28,8 @@
 
       {{ userAddress }}
       <q-btn @click="connectWallet()" v-if="!isConnected"
-        >Connect Your wallet</q-btn
-      >
+        >Connect Your wallet
+      </q-btn>
       <q-input outlined v-model="tipsAmount" label="Tips Amount" />
       <q-btn>Send Tips</q-btn>
     </div>
@@ -32,7 +44,9 @@ import { useWallet } from 'src/composables/wallet'
 import { useAuth, useFirestore } from '@vueuse/firebase'
 import { useRouter } from 'vue-router'
 import { computed, ref, Ref } from 'vue'
-import { doc } from 'firebase/firestore'
+import { deleteDoc, doc } from 'firebase/firestore'
+import { useQuasar } from 'quasar'
+import { timeout } from 'workbox-core/_private'
 
 interface TeamType {
   uid: string
@@ -46,6 +60,7 @@ if (!router.currentRoute.value.params.ulid) {
   router.push('/404')
 }
 
+const $q = useQuasar()
 const appStore = useAppStore()
 const { auth, db } = useFirebase()
 const { isAuthenticated } = useAuth(auth)
@@ -58,6 +73,14 @@ const teamQuery = computed(() => doc(db, 'teams', id as string))
 const teamData = useFirestore(teamQuery, null) as Ref<
   TeamType | undefined | null
 >
+
+const deleteTeam = () => {
+  deleteDoc(doc(db, 'teams', teamData.value?.uid as string))
+
+  $q.notify({ type: 'negative', message: 'Team Remove Successfully' })
+  timeout(3000)
+  router.push('/')
+}
 
 if (!isAuthenticated) {
   // TODO redirect to home page

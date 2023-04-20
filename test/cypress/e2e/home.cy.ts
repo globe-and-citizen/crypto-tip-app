7ef3e4a4cf682824f1fd4c225d1709120eec8506
Hermann EHO <hermannleboss@gmail.com>
2023-04-20 09:52:07 +0000

    test: test create team

--- test/cypress/e2e/home.cy.ts
+++ test/cypress/e2e/home.cy.ts
@@ -28,6 +28,32 @@ describe('Landing', () => {
     cy.get('.q-field__inner > .q-field__control').eq(2).type(randomWallet2.address)
     cy.get('.q-btn--standard').click()
   })
+  it.only('Test Create Writer team', () => {
+    cy.get('.q-pa-md > .q-btn').click()
+    cy.get('.q-toolbar__title').contains('Create New Team')
+
+    // Fill Form
+    cy.get('.q-field__inner > .q-field__control').eq(0).type('Writer Team')
+    cy.get('.q-textarea > .q-field__inner > .q-field__control > .q-field__control-container').type(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget justo tempus, egestas libero vel, pulvinar ' +
+        'lacus. Vestibulum non metus tempus, dictum enim at, finibus justo. Interdum et malesuada fames ac ante ipsum primis' +
+        ' in faucibus. Aenean sodales consequat ultrices. Duis nec elit ultrices, laoreet nulla vitae, aliquam dolor.'
+    )
+    let randomWallet = ethers.Wallet.createRandom()
+    cy.get('.q-field__before > .text-primary').eq(1).click()
+    cy.get('.q-field__inner > .q-field__control').eq(2).type(randomWallet.address)
+    randomWallet = ethers.Wallet.createRandom()
+    cy.get('.q-field__inner > .q-field__control').eq(3).type(randomWallet.address)
+    randomWallet = ethers.Wallet.createRandom()
+    cy.get('.q-field__inner > .q-field__control').eq(4).type(randomWallet.address)
+    cy.get('.q-btn--standard').click()
+
+    //Check the form is submitted successfully
+    cy.get('.q-notification__message').contains('Team successfully Created')
+
+    // Check the redirection
+    cy.get('.q-toolbar__title').contains('Crypto Tips')
+  })
 })
 
 // Workaround for Cypress AE + TS + Vite

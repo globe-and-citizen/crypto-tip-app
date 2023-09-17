7d67c334faeabfc8bbafd12fec7aa8acaaa8f50b
Hermann EHO <hermannleboss@gmail.com>
2023-09-17 07:14:54 +0000

    feat: update verify error message and document verify process

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -13,16 +13,24 @@ const verify = async (req: Request, res: Response) => {
     try {
         const {message, signature} = req.body;
         const SIWEObject = new SiweMessage(message);
+        /**
+         * verify the signature
+         * @param signature
+         * @returns {Promise<SiweResponse>}  This object if valid.
+         * @throws {Error} If the signature is invalid.
+         */
         const {data: newMessage} = await SIWEObject.verify({signature});
+
+        // find the user with the given address in the database 
+        // if not existe create the user and save it in the database using prisma
+        
         // convert newMessage object into a  plain javascript object JSON
         const value = JSON.parse(JSON.stringify(newMessage));
         const token = jwt.sign(value, secretKey, {expiresIn: sessionExpiry});
         res.status(200).json({token});
     } catch (e) {
         console.log('Error: ', e)
-
-        return res.status(500).json({message: 'Error Occur', error: e});
+        return res.status(401).json({message: 'Authentication failed. Please provide valid signature.', error: e});
     }
-    console.log('here 2')
 }
 export {getNonce, verify}
